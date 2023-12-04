55897f2e4ead53763202ae1d3f31248cc6795006
tyler-england <54315363+tyler-england@users.noreply.github.com>
2023-12-04 01:42:16 -0500

    Added a shell file to run instead of the .py file directly

---
+++ example.build-env.sh
@@ -0,0 +1,9 @@
+# create virtual env
+python -m venv venv
+
+# initiate the newly created environment (in your project folder)
+source /Users/.../venv/bin/activate
+#python -c "import sys; print('\n'.join(sys.path))" # see path dirs
+
+# install requirements
+pip install -r /Users/.../requirements.txt
---
+++ example.run.sh
@@ -0,0 +1,6 @@
+# build environment and activate
+/Users/.../build-env.sh
+source /Users/.../venv/bin/activate
+
+# run project file(s)
+python /Users/.../email_forecast.py
