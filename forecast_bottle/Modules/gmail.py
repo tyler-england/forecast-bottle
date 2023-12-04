7110e6c6f35be263cde5853a587bc11aa5125355
tyler-england <54315363+tyler-england@users.noreply.github.com>
2023-12-03 23:51:03 -0500

    Added resolve() method to Path declaration

--- forecast_bottle/Modules/gmail.py
+++ forecast_bottle/Modules/gmail.py
@@ -1,64 +1,69 @@
-import smtplib, ssl
+import smtplib
+import ssl
 from pathlib import Path
 from email.mime.text import MIMEText
 
 
 def send_email(name, sender, time, qty, last_feed, avg_feed, daily_tot, daily_freq):
-    folder = Path(__file__).parents[2]
-    folder = str(folder)
+    folder = Path(__file__).resolve().parents[2]
+    # credentials doc (change as necessary)
+    creds_path = str(folder) + '/credentials.txt'
 
-    doc_path = folder + "/credentials.txt"  # credentials doc (change as necessary)
     try:
-        logindoc = open(doc_path, "r")
+        creds_doc = open(creds_path, 'r')
     except Exception:
-        return "Error: Unable to open credentials doc (" + doc_path + ")"
-    info = [line.rstrip() for line in logindoc]
+        return 'Error: Unable to open credentials doc (' + creds_path + ')'
+    info = [line.rstrip() for line in creds_doc]
 
-    doc_path = folder + "/recipients.txt"  # change as necessary
+    recips_path = str(folder) + '/recipients.txt'  # change as necessary
     try:
-        recipdoc = open(doc_path, "r")
+        recips_doc = open(recips_path, 'r')
     except Exception:
-        return "Error: Unable to find recipients for the email (" + doc_path + ")"
-    recips = [line.rstrip() for line in recipdoc]
+        return 'Error: Unable to find recipients for the email (' + recips_path + ')'
+    recips = [line.rstrip() for line in recips_doc]
 
     for i in range(len(info)):
-        if info[i].lower() == "gmail":
-            x = info[i + 1].find(":") + 1
-            un = info[i + 1][x:]  # get username
-            x = info[i + 2].find(":") + 1
-            pw = info[i + 2][x:]  # get password
-    if un == "" or pw == "":
-        return "Error: username/password info missing"
+        if info[i].lower() == 'gmail':
+            start = info[i + 1].find(':') + 1
+            un = info[i + 1][start:]  # get username
+            start = info[i + 2].find(':') + 1
+            pw = info[i + 2][start:]  # get password
+            break
+    if un == '' or pw == '':
+        return 'Error: username/password info missing'
 
-    time_adj = time.strftime("%b %d, %I:%M %p")
-    last_feed_adj = last_feed.strftime("%b %d, %I:%M %p")
+    time_adj = time.strftime('%b %d, %I:%M %p')
+    last_feed_adj = last_feed.strftime('%b %d, %I:%M %p')
     daily_tot_adj = int(daily_tot)
-    t_delta = str(daily_freq).split(":")
-    if t_delta[2].find(".") > 0:
-        x = t_delta[2].find(".")
+    t_delta = str(daily_freq).split(':')
+    if t_delta[2].find('.') > 0:
+        x = t_delta[2].find('.')
         t_delta[2] = t_delta[2][:x]
-    daily_freq_adj = t_delta[0] + " hrs  " + t_delta[1] + " min  " + t_delta[2] + " sec"
-    body_text = "ESTIMATES"
-    body_text = body_text + "\n\nNext feed: {time}".format(time=time_adj)
-    body_text = body_text + "\n\nAmount: {qty} mL".format(qty=qty)
-    body_text = body_text + "\n\n\nPAST DATA"
-    body_text = body_text + "\n\nLast feed\n{last_feed}".format(last_feed=last_feed_adj)
-    body_text = body_text + "\n\nAvg milk amounts\nday: {daily_tot} mL  |  feed: {avg_feed} mL".format(
-        daily_tot=daily_tot_adj, avg_feed=avg_feed)
-    body_text = body_text + "\n\nAvg time between feeds\n{daily_freq}".format(daily_freq=daily_freq_adj)
+    daily_freq_adj = t_delta[0] + ' hrs  ' + \
+                     t_delta[1] + ' min  ' + t_delta[2] + ' sec'
+    body_text = 'ESTIMATES'
+    body_text = body_text + f'\n\nNext feed: {time_adj}'
+    body_text = body_text + f'\n\nAmount: {qty}'
+    body_text = body_text + '\n\n\nPAST DATA'
+    body_text = body_text + \
+                f'\n\nLast feed\n{last_feed_adj}'
+    body_text = body_text + \
+                f'\n\nAvg milk amounts\nday: {daily_tot_adj} mL  |  feed: {avg_feed} mL'
+    body_text = body_text + f'\n\nAvg time between feeds\n{daily_freq_adj}'
 
-    msg = MIMEText(body_text, "plain")
-    msg['Subject'] = name + "'s Feed Schedule"
+    msg = MIMEText(body_text, 'plain')
+    msg['Subject'] = f'{name}\'s Feed Schedule'
     msg['From'] = sender
-    msg['To'] = ",".join(recips)
+    msg['To'] = ','.join(recips)
 
     port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
+    smtp_server = 'smtp.gmail.com'
     context = ssl.create_default_context()
     try:
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(un, pw)
             server.sendmail(un, recips, msg.as_string())
         return
-    except Exception:
-        return "Error: Unable to send email"
+    except Exception as e:
+        ret = 'Error: Unable to send email:\n' + str(e)
+        return ret
