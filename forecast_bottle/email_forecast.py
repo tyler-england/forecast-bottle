8d8dca85af958862ac36abcc31b9a6a45ba0b110
tyler-england <54315363+tyler-england@users.noreply.github.com>
2023-12-04 00:57:22 -0500

    Removed 'check time' management so program no longer aborts based on time

--- forecast_bottle/email_forecast.py
+++ forecast_bottle/email_forecast.py
@@ -2,88 +2,53 @@ from Modules import keep_data, prediction, gmail
 from pathlib import Path
 import datetime
 
-
-def update_chktime(filepath, dt_chk, fmt):
-    try:
-        with open(chktime, "w") as timedoc:  # check update time (to reduce freq. of Google access & account suspicion)
-            timedoc.write(datetime.datetime.strftime(dt_chk, fmt))
-    except:
-        print("Error: unable to update the next 'check' time in " + filepath)
-    return
-
-chktime = str(Path(__file__).parents[1]) + "/check_time.txt"
-fmt = "%Y-%m-%d %H:%M:%S"
-try:
-    with open(chktime, "r+") as timedoc:  # check update time (to reduce freq. of Google access & account suspicion)
-        chk_time_txt = timedoc.read()
-except:
-    pass
-
-b_cont = False  # boolean to continue with the program
-try:  # find the time & evaluate
-    dt_chk = datetime.datetime.strptime(chk_time_txt, fmt)
-    if datetime.datetime.now() >= dt_chk:
-        b_cont = True
-except Exception:  # something wrong with the datetime(missing, etc.)
-    b_cont = True
-    try:
-        dt_chk
-    except NameError:
-        dt_chk = datetime.datetime.min
-
-if not b_cont:  # retry later
-    print("Aborting due to check_time.txt")
-    quit()
-
-name = "Corwin"  # change baby's name as needed
-sender = "Tyler England"  # change as needed
+name = '<baby name>'  # change baby's name as needed
+sender = '<email sender>'  # change as needed
 
 data = keep_data.get_content(name)  # get historical feed data from Google Keep
-if isinstance(data,str):  # Google refusing to allow access to Keep note
+if isinstance(data, str):  # Google refusing to allow access to Keep note
+    print('Trouble fetching Keep data...')
     print(data)
-    print("Trouble fetching Keep data")
     quit()
 
-time, qty, last_feed, avg_feed, daily_tot, daily_freq = prediction.get_time_qty_summary(data)
+time, qty, bf_mins, last_feed, avg_feed, daily_tot, daily_freq = prediction.get_time_qty_summary(data)
 
 if time == 0 or qty == 0:
-    print("Failed to predict time/qty: " + str(time) + "   " + str(qty))
+    print(f'Failed to predict time/qty: {time} / {qty}')
     quit()
 if time < datetime.datetime.now():  # no longer useful to notify
-    print("Prediction found too late :(")
+    print('Prediction found too late :(')
+    print(time, qty)
     quit()
 
-log = str(Path(__file__).parents[1]) + "/prediction_log.txt"  # if already logged, don't send
+# if already logged, don't send
+log = str(Path(__file__).resolve().parents[1]) + '/prediction_log.txt'
 if not Path(log).exists():
-    with open(log, "w"): pass
+    with open(log, 'w'):
+        pass
 loglines = []
 try:
-    with open(log, "r+") as logdoc:
+    with open(log, 'r+') as logdoc:
         loglines = [line.rstrip() for line in logdoc]
-        chkstr = str(time) + " -- " + str(qty) + "mL"
+        chkstr = f'{time} -- {qty} mL'
         for logline in loglines:
             if logline == chkstr:  # Keep doc hasn't been updated since last check
-                print("No update (no email sent)")
-                if dt_chk < time:
-                    dt_chk = time + datetime.timedelta(minutes=30)  # check again 30 min after feed
-                else:
-                    dt_chk = dt_chk + datetime.timedelta(minutes=30)  # check again in 30 min
-                update_chktime(chktime, dt_chk, fmt)
+                print('No update (already in log -> no email will send)')
                 quit()
         loglines.append(chkstr)
 except Exception:
-    print("Error: Unable to find/open the prediction log (" + log + ")")
+    print('Error: Unable to find/open the prediction log (' + log + ')')
     quit()
 
-x = gmail.send_email(name, sender, time, qty, last_feed, avg_feed, daily_tot, daily_freq)  # create & send email
+qty = f'{qty}mL ({bf_mins} mins BF)'
+x = gmail.send_email(name, sender, time, qty, last_feed, avg_feed,
+                     daily_tot, daily_freq)  # create & send email
 if x is None:
     if loglines:  # add to log
-        with open(log, "w+") as logdoc:
+        with open(log, 'w+') as logdoc:
             for i in range(max(len(loglines) - 6, 0), len(loglines)):
-                logdoc.write(loglines[i] + "\n")
+                logdoc.write(loglines[i] + '\n')
     else:
-        print("Email sent successfully, but prediction log could not be updated")
-    dt_chk = time - datetime.timedelta(minutes=30)  # check for updates 30 min before next feed
-    update_chktime(chktime, dt_chk, fmt)
+        print('Email sent successfully, but prediction log could not be updated')
 else:
     print(x)
